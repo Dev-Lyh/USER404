a371c4d3785a89f6da1f1afefa2c602e4441ade5
EmilyFelicio <emilyfelicio03@gmail.com>
2021-07-06 17:13:09 -0300

    commit com a adição .Net Framework e com o MySqlConnector

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace USER404.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
